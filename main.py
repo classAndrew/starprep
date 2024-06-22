c6459b4d527b8e17556f03464113938dc5561e33
classAndrew <classAndrew@users.noreply.github.com>
2024-06-22 12:14:26 -0700

    add basic functions and .env

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from dotenv import load_dotenv
+from hume import HumeBatchClient
+import os
+
+load_dotenv()
+hume_key = os.environ["hume_key"]
+openai_key = os.environ["openai_key"]
 
 app = Flask(__name__)
+client = HumeBatchClient("<YOUR API KEY>")
+
+
+def gen_next_story_button():
+    pass
+
+def gen_story_from_buttons():
+    pass 
+
+def get_emotions_from_story():
+    pass 
+
+def get_emotion_from_audio():
+    pass
 
 @app.route('/')
 def hello_world():
