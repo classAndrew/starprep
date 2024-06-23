5aacf30ae5ab43012d411660f448ed358c22942d
classAndrew <classAndrew@users.noreply.github.com>
2024-06-23 10:42:39 -0700

    add bg cover

--- frontend/src/components/Form/Confirm.jsx
+++ frontend/src/components/Form/Confirm.jsx
@@ -1,6 +1,8 @@
+import "../../styles/Confirm.css"
+
 function Confirm({ genre, personality, role, mission }) {
   return (
-    <div>
+    <div className="confirm">
       <h1 className="form-header"> Confirm your selection:</h1>
       <ul className="form-btn-list">
         <li className="list-text">Genre: {genre}</li>
